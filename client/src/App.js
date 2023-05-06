452811cb5c45c7b9bf3fba6442bd72778e0e200f
Yash Singh <yash.singh5445@gmail.com>
2023-05-06 14:16:16 +0530

    client part setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
