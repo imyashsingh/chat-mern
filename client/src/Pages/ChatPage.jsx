2c62ee92ea0b94480d31e54093c8de822a3939d6
Yash Singh <yash.singh5445@gmail.com>
2023-05-06 16:24:12 +0530

    created schemas

--- client/src/Pages/ChatPage.jsx
+++ client/src/Pages/ChatPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { HOST } from "../Host";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+  const fetchChats = async () => {
+    const { data } = await axios.get(`${HOST}/api/chats`);
+    setChats(data);
+  };
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
