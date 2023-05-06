101bbd4525126f770ab98c701d588072b8d01e13
Yash Singh <yash.singh5445@gmail.com>
2023-05-06 14:17:26 +0530

    file setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
+var cors = require("cors");
+
+const chats = require("./data/data");
 
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("api");
+  res.send("API is running");
 });
 
-app.listen(5000, () => {
+app.get("/api/chats", (req, res) => {
+  res.json(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const chat = chats.find((ele) => ele._id === req.params.id);
+  res.json(chat);
+});
+
+app.listen(PORT, () => {
   console.log("server is up");
 });
